b72be23c1c9727be8bd19b9cb3d6422b8c13fdcd
mWarned <mihai.jizdan2021@gmail.com>
2023-01-02 20:24:55 +0200

    Bot start message and reply full dataframes

--- bot.py
+++ bot.py
@@ -2,18 +2,55 @@ import excelRead
 import logging
 from aiogram import Bot, Dispatcher, executor, types
 
-#log level
+# log level
 logging.basicConfig(level=logging.INFO)
 
-#bot init
+# bot init
 bot = Bot(token="5810303748:AAHi32NfjbjfObnFznVj6GH92OLKwNuWea0")
 dp = Dispatcher(bot)
 
-#echo
-@dp.message_handler()
-async def echo(message: types.Message):
-    await message.answer(message.text)
+# buttons
+luni = types.KeyboardButton("Luni")
+marti = types.KeyboardButton("Marti")
+miercuri = types.KeyboardButton("Miercuri")
+joi = types.KeyboardButton("Joi")
+vineri = types.KeyboardButton("Vineri")
 
-#run long-polling
+# keyboards
+keyboard1 = types.ReplyKeyboardMarkup(row_width=3, resize_keyboard=True, one_time_keyboard=True).add(luni, marti, miercuri, joi, vineri)
+
+
+# start message handling
+@dp.message_handler(commands=["start", "help"])
+async def welcomeText(message: types.Message):
+    await bot.send_photo(message.chat.id,
+                         'https://i.pinimg.com/originals/c1/2d/af/c12daffa996683fe1080c809aca58e23.png')
+
+    await bot.send_message(message.chat.id,
+                           "Salutare {0.first_name}!\n Ai nevoie de temele pentru acasă? Eu am toate temele pentru acasă de care ai nevoie. Scrie <b>/teme</b> pentru a afla temele".format(
+                               message.from_user), parse_mode='html')
+
+
+@dp.message_handler(commands=["teme"])
+async def popMenu(message: types.Message):
+    await message.reply("Temele pe ce zi dorii să aflați?", reply_markup=keyboard1)
+
+
+@dp.message_handler()
+async def answerToUser(message: types.Message):
+
+    if message.text == "Luni":
+        await message.answer(excelRead.dfLuni)
+    elif message.text == "Marti":
+        await message.answer(excelRead.dfMarti)
+    elif message.text == "Miercuri":
+        await message.answer(excelRead.dfMiercuri)
+    elif message.text == "Joi":
+        await message.answer(excelRead.dfJoi)
+    elif message.text == "Vineri":
+        await message.answer(excelRead.dfVineri)
+
+
+# run long-polling
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=False)
--- excelRead.py
+++ excelRead.py
@@ -1,13 +1,13 @@
 import pandas as pd
 
-#the sheet with the homework
+# the sheet with the homework
 sheet_id = '1XT36KHawZKJJ3r-W3GHJkOflrQksZ_aUrsxOH8-bwbY'
 
-#dataframe
+# dataframe
 df = pd.read_csv(f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv")
 
 
-#smaller dataframes for each day
+# smaller dataframes for each day
 dfLuni = df.iloc[0:10, 0:2]
 dfMarti = df.iloc[0:10, 3:5]
 dfMiercuri = df.iloc[0:10, 6:8]
